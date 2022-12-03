b334003e6aa53b1835dca7cd8c632cbff3a3836b
Thái Nguyên <you@example.com>
2022-12-03 17:55:27 +0700

    upload03/12/2022

---
+++ Bai1 nhapXuatTrongJava/src/Hello.java
@@ -0,0 +1,9 @@
+
+public class Hello {
+    //main => ctrl + khoang trang => enter
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+		//sysout => ctrl + khoang trang => enter
+		
+}
